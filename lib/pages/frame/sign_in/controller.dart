dfb9a6864ce06e58ecb1fd5c8fadb0ff5fdb06da
altrono <hbotema16@gmail.com>
2023-02-26 18:32:30 +0200

    Added sign in module controller , binding and library

---
+++ lib/pages/frame/sign_in/controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:video_call_chat_app/pages/frame/sign_in/state.dart';
+
+
+class SignInController extends GetxController {
+  SignInController();
+  final state = SignInState();
+}
---
+++ lib/pages/frame/sign_in/view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_call_chat_app/pages/frame/welcome/controller.dart';
+
+class SignInPage extends GetView<WelcomeController> {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
