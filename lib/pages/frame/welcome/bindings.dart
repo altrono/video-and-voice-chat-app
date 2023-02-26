7bfdfbb6d31d59e1b8171505e74aa52d87e33580
altrono <hbotema16@gmail.com>
2023-02-26 12:15:39 +0200

    Added Welcome module and binding controller

---
+++ lib/pages/frame/welcome/bindings.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:video_call_chat_app/pages/frame/welcome/controller.dart';
+
+class WelcomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<WelcomeController>(() => WelcomeController());
+  }
+}
