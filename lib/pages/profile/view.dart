bfc1ccb7bd9d847d6657a3b8cc99aab1696e4859
altrono <hbotema16@gmail.com>
2023-03-27 19:18:20 +0200

    added Profile complete button  part 2

--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -10,10 +10,125 @@ import 'controller.dart';
 class ProfilePage extends GetView<ProfileController> {
   const ProfilePage({Key? key}) : super(key: key);
 
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text(
+        'Profile',
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontSize: 26.sp,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
+  }
+  
+  Widget _buildProfilePhoto() {
+    return Stack(
+      children: [
+        Container(
+          width: 120.w,
+          height: 120.w,
+          decoration: BoxDecoration(
+            color: AppColors.primarySecondaryBackground,
+            borderRadius: BorderRadius.all(Radius.circular(60.w)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1)
+              )
+            ]
+          ),
+          child: Image(
+            width: 120.w,
+            height: 120.h,
+            image: AssetImage(
+              'assets/images/account_header.png'
+            ),
+          ),
+        ),
+        Positioned(
+            bottom: 20.w,
+            right: 0.w,
+            height: 35.w,
+            child: GestureDetector(
+              child: Container(
+                height: 35.w,
+                width: 35.w,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryElement,
+                  borderRadius: BorderRadius.all(Radius.circular(40.w),
+                  ),
+                ),
+                child: Image.asset('assets/icons/edit.png'),
+              ),
+            ),
+        ),
+
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: _buildAppBar(),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                child: Column(
+                  children: [
+                    _buildProfilePhoto(),
+                    _buildCompleteBtn(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget _buildCompleteBtn() {
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(top: 60.h, bottom: 30.h),
+        decoration: BoxDecoration(
+          color: AppColors.primaryElement,
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1),
+
+            )
+          ]
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Complete',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.primaryElementText,
+                fontWeight: FontWeight.normal,
+                fontSize: 14.sp,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
