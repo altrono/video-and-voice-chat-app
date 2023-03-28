4c97699a21087094c321321bd37b551dd07b4410
altrono <hbotema16@gmail.com>
2023-03-28 17:12:31 +0200

    added Profile UI final

--- lib/pages/profile/controller.dart
+++ lib/pages/profile/controller.dart
@@ -1,10 +1,17 @@
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:video_call_chat_app/common/store/store.dart';
 import 'package:video_call_chat_app/pages/profile/state.dart';
 
 class ProfileController extends GetxController {
   ProfileController();
 
   final state = ProfileState();
+
+  Future<void> goLogout() async {
+    await GoogleSignIn().signOut();
+    await UserStore.to.onLogout();
+  }
 
 
 }
--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -45,19 +45,21 @@ class ProfilePage extends GetView<ProfileController> {
           child: Image(
             width: 120.w,
             height: 120.h,
+            fit: BoxFit.cover,
             image: AssetImage(
               'assets/images/account_header.png'
             ),
           ),
         ),
         Positioned(
-            bottom: 20.w,
+            bottom: 0.w,
             right: 0.w,
             height: 35.w,
             child: GestureDetector(
               child: Container(
                 height: 35.w,
                 width: 35.w,
+                padding: EdgeInsets.all(7.w),
                 decoration: BoxDecoration(
                   color: AppColors.primaryElement,
                   borderRadius: BorderRadius.all(Radius.circular(40.w),
@@ -85,6 +87,8 @@ class ProfilePage extends GetView<ProfileController> {
                   children: [
                     _buildProfilePhoto(),
                     _buildCompleteBtn(),
+                    _buildLogoutBtn()
+
                   ],
                 ),
               ),
@@ -129,6 +133,57 @@ class ProfilePage extends GetView<ProfileController> {
           ],
         ),
       ),
+    );
+  }
+
+
+  Widget _buildLogoutBtn(){
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(top: 0.h, bottom: 30.h),
+        decoration: BoxDecoration(
+            color: AppColors.primarySecondaryElementText,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+
+              )
+            ]
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Logout',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.primaryText,
+                fontWeight: FontWeight.normal,
+                fontSize: 14.sp,
+              ),
+            )
+          ],
+        ),
+      ),
+      onTap: (){
+        Get.defaultDialog(
+          title: 'Are you sure you want to log out?',
+          content: Container(),
+          onConfirm: (){
+            controller.goLogout();
+          },
+          onCancel: (){},
+          textConfirm: 'Confirm',
+          textCancel: 'Cancel',
+          confirmTextColor: Colors.white
+        );
+      },
     );
   }
 }
